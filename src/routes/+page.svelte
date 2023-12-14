670b827a71ee474ae1fbe021a6319b3507a85e56
Hugo Arnlund <hugomelo04@gmail.com>
2023-12-14 12:42:50 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+
+<div class="flex justify-center">
+    <div class="flex flex-col items-center mt-20 w-full max-w-sm">
+
+        <span class="font-light text-5xl select-none">Melo.se</span>
+
+        <div class="w-9/12 mt-12 flex flex-col gap-3 lg:text-center">
+            <span class="font-medium text-lg select-none">Projekt</span>
+
+            <a class="link ml-4" href="/twister">Twister Spinner</a>
+            <a class="link ml-4" href="/twister">Värmepumps fjärrkontroll</a>
+        </div>
+
+
+        <div class="w-9/12 flex flex-col gap-3 mt-10 lg:text-center">
+            <span class="font-medium text-lg select-none">Andra sidor</span>
+
+            <a class="link ml-4" href="https://github.com/TheMole3">Github</a>
+        </div>
+
+
+    </div>
+</div>
