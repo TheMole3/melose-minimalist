2c07ce2c94df10cba488e00495e9a6d089f56c10
Hugo Arnlund <hugomelo04@gmail.com>
2024-11-16 11:55:19 +0100

    Fixed alignment issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,14 +7,14 @@
         <div class="w-9/12 mt-12 flex flex-col gap-3 lg:text-center">
             <span class="font-medium text-lg select-none">Projekt</span>
 
-            <a class="link ml-4" rel="external" href="https://twister.melo.se/">Twister Spinner</a>
+            <a class="link ml-4 lg:ml-0" rel="external" href="https://twister.melo.se/">Twister Spinner</a>
         </div>
 
 
         <div class="w-9/12 flex flex-col gap-3 mt-10 lg:text-center">
             <span class="font-medium text-lg select-none">Andra sidor</span>
 
-            <a class="link ml-4" rel="external" href="https://github.com/TheMole3">Github</a>
+            <a class="link ml-4 lg:ml-0" rel="external" href="https://github.com/TheMole3">Github</a>
         </div>
 
 
